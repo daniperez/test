33ef153f6df3cc19d858565ba9ddca08c09bd8b9
Dani Perez <daniperez@users.sourceforge.net>
2014-09-07 21:22:49 +0200

    starting to forget how java works

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,5 +1,7 @@
 
-public static void main(String[] args) {
+class Foo {
+    public static void main(String[] args) {
 
-    System.out.println("Foo!");
+        System.out.println("Foo!");
+    }
 }
